205da754dbc5b727826bdb1b7f6a775233107a16
蒼時弦也 <elct9620@frost.tw>
2017-10-25 16:42:05 +0800

    Add admin user test

--- spec/models/admin_user_spec.rb
+++ spec/models/admin_user_spec.rb
@@ -3,5 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe AdminUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email) { 'admin@example.com' }
+
+  describe '#display_name' do
+    it 'use email account as display name' do
+      user = AdminUser.new(email: email)
+      expect(user.display_name).to eq(email.split('@').first)
+    end
+  end
+
+  describe '#avatar' do
+    it 'generate gravatar url' do
+      hash = Digest::MD5.hexdigest(email)
+      user = AdminUser.new(email: email)
+      expect(user.avatar).to include(hash)
+    end
+  end
 end
