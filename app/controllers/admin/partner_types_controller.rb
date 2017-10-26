3a3d5e4a089d053bb090e4f91a761100af6b8bf6
蒼時弦也 <elct9620@frost.tw>
2017-10-26 10:21:42 +0800

    Add edit partner type feature

--- app/controllers/admin/partner_types_controller.rb
+++ app/controllers/admin/partner_types_controller.rb
@@ -2,6 +2,8 @@
 
 module Admin
   class PartnerTypesController < Admin::BaseController
+    before_action :find_type, except: %i[index new create]
+
     def index
       @types = PartnerType.all
     end
@@ -16,10 +18,23 @@ module Admin
       render :new
     end
 
+    def edit; end
+
+    def update
+      if @type.update(partner_type_params)
+        return redirect_to admin_partner_types_path
+      end
+      render :edit
+    end
+
     private
 
+    def find_type
+      @type = PartnerType.find(params[:id])
+    end
+
     def partner_type_params
-      params.require(:partner_type).permit(:name)
+      params.require(:partner_type).permit(:name, :locale)
     end
   end
 end
