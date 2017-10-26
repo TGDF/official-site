a931b7ed027da52e082b299f3ed555ee1e1af6d8
蒼時弦也 <elct9620@frost.tw>
2017-10-26 11:42:11 +0800

    Add basic CRUD test for partner type

--- spec/features/admin/admin_partner_types_spec.rb
+++ spec/features/admin/admin_partner_types_spec.rb
@@ -18,4 +18,38 @@ RSpec.feature 'Admin::PartnerTypes', type: :feature do
       @types.each { |type| expect(page).to have_content(type.name) }
     end
   end
+
+  describe '#new' do
+    it 'can add new partner type' do
+      visit new_admin_partner_type_path
+      fill_in 'partner_type_name', with: 'Example'
+      click_button '新增Partner type'
+      expect(page).to have_content('Example')
+    end
+  end
+
+  describe '#edit' do
+    before { @type = create(:partner_type) }
+
+    it 'can edit partner type' do
+      visit edit_admin_partner_type_path(@type)
+      fill_in 'partner_type_name', with: 'New Type Name'
+      click_button '更新Partner type'
+      expect(page).to have_content('New Type Name')
+    end
+  end
+
+  describe '#destroy' do
+    before { @type = create(:partner_type) }
+
+    it 'can destroy partner type' do
+      visit admin_partner_types_path
+
+      within first('td', text: @type.name).first(:xpath, './/..') do
+        click_on 'Destroy'
+      end
+
+      expect(page).not_to have_content(@type.name)
+    end
+  end
 end
