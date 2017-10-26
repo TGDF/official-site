b094364dc77d18ee77b1f86fd7ddb962c3893d3a
蒼時弦也 <elct9620@frost.tw>
2017-10-26 10:13:09 +0800

    Add create partner types feature

--- app/controllers/admin/partner_types_controller.rb
+++ app/controllers/admin/partner_types_controller.rb
@@ -5,5 +5,21 @@ module Admin
     def index
       @types = PartnerType.all
     end
+
+    def new
+      @type = PartnerType.new
+    end
+
+    def create
+      @type = PartnerType.create(partner_type_params)
+      return redirect_to admin_partner_types_path if @type.save
+      render :new
+    end
+
+    private
+
+    def partner_type_params
+      params.require(:partner_type).permit(:name)
+    end
   end
 end
--- app/models/partner_type.rb
+++ app/models/partner_type.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
 class PartnerType < ApplicationRecord
+  validates :name, presence: true
 end
