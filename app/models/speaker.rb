b025b9b9f2a8c156c83148d20f7368adecdc7baa
蒼時弦也 <elct9620@frost.tw>
2017-11-02 00:33:45 +0800

    Prepare uploader for speaker

--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class Speaker < ApplicationRecord
+  # FIXME: If globalize for rails 5 is ready, prevent to add `attribute`
+  attribute :name
+  attribute :description
   translates :name, :description
+
+  mount_uploader :logo, LogoUploader
 
   validates :name, :description, :avatar, presence: true
 end
---
+++ app/uploaders/avatar_uploader.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  # storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{Apartment::Tenant.current}" \
+      "/#{model.class.to_s.underscore}" \
+      "/#{mounted_as}/#{model.id}"
+  end
+
+  process resize_to_fit: [400, 400]
+
+  # version :thumb do
+  #   process resize_to_fit: [50, 50]
+  # end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  def extension_whitelist
+    %w[jpg jpeg gif png]
+  end
+end
