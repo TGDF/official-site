3786f4468f02608667ce3bcb99ccfe1f92f81f39
蒼時弦也 <elct9620@frost.tw>
2018-03-11 17:03:36 +0800

    Apply basic theme

---
+++ app/helpers/navbar_helper.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module NavbarHelper
+  def current_path_under?(path)
+    path = url_for(path)
+    return false if path == admin_root_path && request.path != path
+    uri = URI(path)
+    current_path_match?(uri) && current_params_under?(uri)
+  end
+
+  def current_path_match?(uri)
+    return request.path.start_with?(uri.path) if params[:id].present?
+    request.path == uri.path
+  end
+
+  def current_params_under?(uri)
+    (URI.decode_www_form(uri.query || '') - request.params.to_a).empty?
+  end
+
+  def nav_item(name, path, options = {})
+    style = []
+    style << 'current' if current_path_under?(path)
+    content_tag :li, class: style.join(' ') do
+      link_to name, path, options
+    end
+  end
+end
