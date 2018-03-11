785151af92e9535f1f2f59982846a2207c1ba519
蒼時弦也 <elct9620@frost.tw>
2018-03-11 22:00:33 +0800

    Disable Rollbaar JavaScript catch feature

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -4,13 +4,4 @@ Rollbar.configure do |config|
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
   config.enabled = false unless Rails.env.production?
   config.environment = ENV['ROLLBAR_ENV'].presence || Rails.env
-
-  config.js_enabled = true if Rails.env.production?
-  config.js_options = {
-    accessToken: ENV['ROLLBAR_ACCESS_TOKEN'],
-    captureUncaught: true,
-    payload: {
-      environment: ENV['ROLLBAR_ENV'].presence || Rails.env
-    }
-  }
 end
