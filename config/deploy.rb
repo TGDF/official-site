9f288e0c5c39e76eb022fece3f4d176c851620c1
蒼時弦也 <elct9620@frost.tw>
2018-03-16 00:33:10 +0800

    Add missing settings.yml config

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/settings.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'node_modules'
