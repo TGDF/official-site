b974aae4ba0382df866ea8b5153c8d1274147796
蒼時弦也 <elct9620@frost.tw>
2018-03-16 01:47:21 +0800

    Prevent test environment shared session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_tgdf_session', domain: Settings.site.default_domain, tld_length: 1
+Rails.application.config.session_store :cookie_store, key: '_tgdf_session', domain: Settings.site.default_domain, tld_length: 1 unless Rails.env.test?
