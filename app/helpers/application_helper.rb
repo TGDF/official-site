0a844d3baff7df868e8cf7cd0631e1045a2c65cd
蒼時弦也 <elct9620@frost.tw>
2018-03-18 15:29:46 +0800

    Improve page title display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
     return if current_path_under?(root_path)
     render 'shared/breadcrumb'
   end
+
+  def page_title
+    [content_for(:page_title), site_name].compact.join(' | ')
+  end
+
+  def site_name
+    current_site.name || t('site_name')
+  end
 end
