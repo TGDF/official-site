654321025bebd0642b1f8e572899dec991e502de
蒼時弦也 <elct9620@frost.tw>
2018-03-18 15:20:47 +0800

    Setup news list page

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class NewsController < ApplicationController
+  def index
+    @page_name = I18n.t('news.index.title')
+    # TODO: Specify page background
+    @news = News.published.all
+  end
+
   def show
     @news = News.find(params[:id])
     @page_name = @news.title
