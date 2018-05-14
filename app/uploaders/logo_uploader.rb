9a424a33608d5c453ec231d15e9b49faff853352
蒼時弦也 <elct9620@frost.tw>
2018-05-14 23:06:20 +0800

    Prepare for partner display for home page

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -18,16 +18,16 @@ class LogoUploader < CarrierWave::Uploader::Base
   end
 
   # Process files as they are uploaded:
-  process resize_to_fit: [270, 120]
+  # process resize_to_fill: [270, 120]
   #
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :v1 do
+    process resize_to_fill: [270, 120]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
