30328cc2a92458fefc1dce329ba61d51f9fd3994
蒼時弦也 <elct9620@frost.tw>
2018-06-17 22:45:22 +0800

    Fix validator for agenda time

--- app/models/agenda_time.rb
+++ app/models/agenda_time.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AgendaTime < ApplicationRecord
-  validates :order, presence: true
+  validates :label, presence: true
 
   default_scope -> { order(order: :asc) }
 end
