84f4f24f448773b3ac03e377fa6971d35ed80d0a
蒼時弦也 <elct9620@frost.tw>
2018-06-18 21:12:20 +0800

    Add agenda link with room and time

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -6,5 +6,9 @@ class Agenda < ApplicationRecord
   has_many :agendas_speakers, dependent: :destroy
   has_many :speakers, through: :agendas_speakers
 
+  belongs_to :time, class_name: 'AgendaTime', foreign_key: :time_id,
+                    inverse_of: :agendas, optional: true
+  belongs_to :room, optional: true
+
   validates :subject, :description, presence: true
 end
--- app/models/agenda_time.rb
+++ app/models/agenda_time.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class AgendaTime < ApplicationRecord
+  has_many :agendas, dependent: :nullify
   belongs_to :day, class_name: 'AgendaDay', inverse_of: :times
 
   validates :label, presence: true
 
   default_scope -> { order(order: :asc) }
+
+  def name
+    "#{day.label} - #{label}"
+  end
 end
