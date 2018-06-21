f1df6390aacf446e44240abb22183301a66242ea
蒼時弦也 <elct9620@frost.tw>
2018-06-21 17:02:11 +0800

    Fix cookie key conflict

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_tgdf_session', domain: Settings.site.default_domain, tld_length: 1 unless Rails.env.test?
+Rails.application.config.session_store :cookie_store, key: "_tgdf_session_#{Rails.env}", domain: ".#{Settings.site.default_domain}" unless Rails.env.test?
