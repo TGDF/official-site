da166071c1baed8ad527a87609b69e3489306219
蒼時弦也 <elct9620@frost.tw>
2018-07-08 10:16:08 +0800

    Add basic game CRUD in admin panel

---
+++ app/controllers/admin/games_controller.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Admin
+  class GamesController < Admin::BaseController
+    before_action :find_game, only: %i[edit update destroy]
+
+    def index
+      @games = Game.all
+    end
+
+    def new
+      @game = Game.new
+    end
+
+    def create
+      Mobility.with_locale(I18n.default_locale) do
+        return redirect_to admin_games_path if Game.create(game_params)
+        render :new
+      end
+    end
+
+    def edit; end
+
+    def update
+      Mobility.with_locale(admin_current_resource_locale) do
+        return redirect_to admin_games_path if @game.update(game_params)
+        render :edit
+      end
+    end
+
+    def destroy
+      @game.destroy
+      redirect_to admin_games_path
+    end
+
+    private
+
+    def find_game
+      @game = Game.find(params[:id])
+    end
+
+    def game_params
+      params
+        .require(:game)
+        .permit(:name, :description, :team, :video, :thumbnail)
+    end
+  end
+end
