3a95013460195a2b86307d78cb886b5f0f39cf52
蒼時弦也 <elct9620@frost.tw>
2018-07-09 21:26:10 +0800

    Add link to game, Closed #141

--- app/helpers/theme_helper.rb
+++ app/helpers/theme_helper.rb
@@ -4,4 +4,11 @@ module ThemeHelper
   def fa_icon(name)
     content_tag :i, '', class: "fa fa-#{name}"
   end
+
+  def label_with_icon(name, icon)
+    capture do
+      concat fa_icon(icon)
+      concat name
+    end
+  end
 end
---
+++ db/migrate/20180709130737_add_website_to_games.rb
@@ -0,0 +1,5 @@
+class AddWebsiteToGames < ActiveRecord::Migration[5.1]
+  def change
+    add_column :games, :website, :string
+  end
+end
