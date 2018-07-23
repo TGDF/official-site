bfb18a63f0bb96853090fc040693e3b656e15995
yanglin <yanglin5689446@gmail.com>
2018-07-23 17:55:07 +0800

    Add day tab to agenda@show

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -2,6 +2,10 @@
 
 class AgendasController < ApplicationController
   def show
-    @days = AgendaDay.all.includes(times: { agendas: %i[room speakers] })
+    @days = AgendaDay.all
+    q = params[:day].try(:to_i)
+    @query_day = AgendaDay.exists?(q) ? q : @days.first.id
+    @day = AgendaDay.includes(times: { agendas: %i[room speakers] })
+                    .find(@query_day)
   end
 end
