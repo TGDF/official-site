23585a2ddefe96c9115e2a438baff61f72ea0bc1
yanglin <yanglin5689446@gmail.com>
2018-07-24 14:01:51 +0800

    Change tab switch method from SSR to js render

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -2,10 +2,6 @@
 
 class AgendasController < ApplicationController
   def show
-    @days = AgendaDay.all
-    q = params[:day].try(:to_i)
-    @query_day = AgendaDay.exists?(q) ? q : @days.first.id
-    @day = AgendaDay.includes(times: { agendas: %i[room speakers] })
-                    .find(@query_day)
+    @days = AgendaDay.all.includes(times: { agendas: %i[room speakers] })
   end
 end
