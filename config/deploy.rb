e4bfcc7d475121e3a26d3438104e16f20336163b
蒼時弦也 <elct9620@frost.tw>
2019-04-11 20:34:25 +0800

    Prevent whenever override

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,3 +39,5 @@ set :default_env, path: '/usr/local/ruby-2.5.0/bin:$PATH'
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
