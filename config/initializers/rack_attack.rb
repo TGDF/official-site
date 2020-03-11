d82a2344e9d2cf761d3ce02e545e81194e3de2a9
蒼時弦也 <elct9620@frost.tw>
2020-03-11 16:52:03 +0800

    Adjsut rack-attack throttle

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
-# :nodoc:
-module Rack
-  class Attack
-    throttle('admin/sign_in', limit: 5, period: 1.minute) do |req|
-      req.ip if req.path == '/admin/sign_in' && req.post?
-    end
+Rack::Attack.throttle('admin/sign_in', limit: 20, period: 1.hour) do |req|
+  req.ip if req.path == '/admin/sign_in' && req.post?
+end
+
+ActiveSupport::Notifications.subscribe('rack.attack') do |_name, _start, _finish, _request_id, req|
+  Rails.logger.tagged('Rack::Attack') do
+    Rails.logger.warn "Throttled #{req.env['rack.attack.match_discriminator']}"
   end
 end
