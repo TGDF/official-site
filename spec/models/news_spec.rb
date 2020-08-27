5211f72235228b9f55202cfe3817c9f4116a6585
蒼時弦也 <elct9620@frost.tw>
2020-08-27 22:35:57 +0800

    Skip news slug uniqueness check

--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -2,11 +2,12 @@
 
 require 'rails_helper'
 
-RSpec.describe(News, type: :model) do
-  it { is_expected.to(belong_to(:author)) }
+RSpec.describe News, type: :model do
+  subject { build(:news) }
+  it { is_expected.to belong_to(:author) }
 
-  it { is_expected.to(validate_presence_of(:title)) }
-  it { is_expected.to(validate_presence_of(:content)) }
-  it { is_expected.to(validate_presence_of(:slug)) }
-  it { is_expected.to(validate_uniqueness_of(:slug)) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:content) }
+  it { is_expected.to validate_presence_of(:slug) }
+  it { is_expected.to validate_uniqueness_of(:slug) }
 end
