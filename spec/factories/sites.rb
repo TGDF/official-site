42c98cec28450a7d0fef192495ebfb022d736920
蒼時弦也 <elct9620@frost.tw>
2020-08-27 22:40:47 +0800

    Fix factory generated site tenant name invalid

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -4,6 +4,6 @@ FactoryBot.define do
   factory :site do
     name { Faker::Name.name }
     sequence(:domain) { |id| "#{id}.#{Faker::Internet.domain_name}" }
-    sequence(:tenant_name) { |id| "#{Faker::Internet.domain_word}_#{id}" }
+    sequence(:tenant_name) { |id| "tgdf_#{id}" }
   end
 end
