17fb67f1a9b515a76c59f26afd5bbd9b283034be
蒼時弦也 <elct9620@frost.tw>
2020-09-17 23:28:14 +0800

    Update dashboard view

--- app/helpers/admin/navbar_helper.rb
+++ app/helpers/admin/navbar_helper.rb
@@ -2,13 +2,10 @@
 
 module Admin
   module NavbarHelper
-    def admin_navbar_site_item(name, host, icon: 'square')
-      tag.li do
-        link_to admin_root_url(host: host) do
-          concat fa_icon(icon)
-          concat ' '
-          concat name
-        end
+    def admin_navbar_site_item(name, host, icon: 'cil-square')
+      link_to admin_root_url(host: host), class: 'dropdown-item' do
+        concat svg_icon('media/sprites/free.svg', icon, class: 'c-icon mfe-2')
+        concat name
       end
     end
   end
