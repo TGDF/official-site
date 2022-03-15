56c6598cb57af70325857fed071989e1f635d254
蒼時弦也 <elct9620@frost.tw>
2022-03-15 19:58:38 +0800

    Configure CarrierWave to allow use S3 mode

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,21 @@
 # frozen_string_literal: true
 
 CarrierWave.configure do |config|
-  config.storage = :file
+  if Settings.s3.enabled == 'yes'
+    config.storage = :fog
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Settings.s3.access_key_id,
+      aws_secret_access_key: Settings.s3.secret_access_key,
+      region: Settings.s3.region
+    }
+    config.fog_directory  = Settings.s3.bucket
+    config.fog_public     = true
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  else
+    config.storage = :file
+  end
+
   config.enable_processing = false if Rails.env.test?
   config.asset_host = Settings.assets.host
 end
