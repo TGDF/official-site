cf80b97da960c3fc490760ca2159e1637597bd3a
蒼時弦也 <elct9620@frost.tw>
2022-03-30 19:10:24 +0800

    Replace Rollbar to Sentry

--- config/initializers/rollbar.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Rollbar.configure do |config|
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
-  config.enabled = false unless Rails.env.production?
-  config.environment = ENV['ROLLBAR_ENV'].presence || Rails.env
-end
