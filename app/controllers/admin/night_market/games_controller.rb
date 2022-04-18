b22ca0dad9c8019022f196b810f57195c53c29c5
蒼時弦也 <elct9620@frost.tw>
2022-04-18 19:08:16 +0800

    Add create night market game controller action

--- app/controllers/admin/night_market/games_controller.rb
+++ app/controllers/admin/night_market/games_controller.rb
@@ -10,6 +10,23 @@ module Admin
       def new
         @game = ::NightMarket::Game.new
       end
+
+      def create
+        Mobility.with_locale(I18n.default_locale) do
+          return redirect_to(admin_night_market_games_path) if ::NightMarket::Game.create(game_params)
+
+          render :new
+        end
+      end
+
+      private
+
+      def game_params
+        params
+          .require(:night_market_game)
+          .permit(:name, :description, :team, :video, :order,
+                  :thumbnail, :website, :remove_thumbnail)
+      end
     end
   end
 end
