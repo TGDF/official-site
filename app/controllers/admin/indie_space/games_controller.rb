4e19ee462d3229e4353cc26cdf597a280a1ad137
蒼時弦也 <elct9620@frost.tw>
2022-04-19 19:41:24 +0800

    Refactor to reuse Admin::GamesController for IndieSpace and NightMarket

--- app/controllers/admin/indie_space/games_controller.rb
+++ app/controllers/admin/indie_space/games_controller.rb
@@ -2,45 +2,8 @@
 
 module Admin
   module IndieSpace
-    class GamesController < Admin::BaseController
-      include Admin::HasGame
-
-      before_action :find_game, only: %i[edit update destroy]
-
-      def index
-        @games = ::IndieSpace::Game.all
-      end
-
-      def new
-        @game = ::IndieSpace::Game.new
-      end
-
-      def create
-        Mobility.with_locale(I18n.default_locale) do
-          return redirect_to(admin_indie_space_games_path) if ::IndieSpace::Game.create(game_params)
-
-          render :new
-        end
-      end
-
-      def edit; end
-
-      def update
-        Mobility.with_locale(admin_current_resource_locale) do
-          return redirect_to(admin_indie_space_games_path) if @game.update(game_params)
-
-          render :edit
-        end
-      end
-
-      def destroy
-        @game.destroy
-        redirect_to(admin_indie_space_games_path)
-      end
-
-      private
-
-      def resouce
+    class GamesController < Admin::GamesController
+      def resource
         ::IndieSpace::Game
       end
     end
--- app/controllers/admin/night_market/games_controller.rb
+++ app/controllers/admin/night_market/games_controller.rb
@@ -2,35 +2,7 @@
 
 module Admin
   module NightMarket
-    class GamesController < Admin::BaseController
-      include Admin::HasGame
-
-      def index
-        @games = ::NightMarket::Game.all
-      end
-
-      def new
-        @game = ::NightMarket::Game.new
-      end
-
-      def create
-        Mobility.with_locale(I18n.default_locale) do
-          return redirect_to(admin_night_market_games_path) if ::NightMarket::Game.create(game_params)
-
-          render :new
-        end
-      end
-
-      def update
-        Mobility.with_locale(admin_current_resource_locale) do
-          return redirect_to(admin_night_market_games_path) if @game.update(game_params)
-
-          render :edit
-        end
-      end
-
-      private
-
+    class GamesController < Admin::GamesController
       def resource
         ::NightMarket::Game
       end
