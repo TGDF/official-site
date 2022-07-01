5336fb4c3ce50b859e824a8a88f326b2cee3f146
蒼時弦也 <elct9620@frost.tw>
2022-07-01 19:40:55 +0800

    Fix cookie store in locahost domain cannot login

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,7 +4,7 @@ unless Rails.env.test?
   Rails.application.config.session_store(
     :cookie_store,
     key: Settings.site.session_key,
-    domain: %W[.#{Settings.site.default_domain} #{Settings.site.default_domain}],
+    domain: Rails.env.production? ? %W[.#{Settings.site.default_domain} #{Settings.site.default_domain}] : nil,
     secure: Rails.env.production?
   )
 end
