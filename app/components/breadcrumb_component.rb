e96db41e8853fb7c41a380bacf5bc3b56c018762
蒼時弦也 <elct9620@frost.tw>
2022-08-25 19:51:37 +0800

    Refactor breadcrumb to BreadcrumbComponent

---
+++ app/components/breadcrumb_component.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class BreadcrumbComponent < ViewComponent::Base
+  delegate_missing_to :@breadcrumbs
+
+  def initialize(breadcrumbs:)
+    super
+    @breadcrumbs = breadcrumbs
+  end
+
+  def render?
+    return if current_page?('/')
+    return if content_for(:disable_breadcrumb).present?
+
+    any?
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def render_breadcrumb
-    return if current_page?(root_path)
-    return if current_page?('/')
-    return if content_for(:disable_breadcrumb).present?
-
-    render('shared/breadcrumb')
-  end
-
   def site_name
     current_site.name || t('site_name')
   end
