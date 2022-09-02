986ea49ec4ae50c6ee90f597a6fa347ffa1bbede
蒼時弦也 <elct9620@frost.tw>
2022-09-02 19:23:36 +0800

    Update index partner logo list display

---
+++ app/components/partner_logo_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class PartnerLogoComponent < ViewComponent::Base
+  with_collection_parameter :partner
+
+  delegate_missing_to :@partner
+
+  def initialize(partner:)
+    super
+    @partner = partner
+  end
+end
---
+++ spec/components/partner_logo_component_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe PartnerLogoComponent, type: :component do
+  let(:partner) { create(:partner, name: '唯晶科技', url: 'https://www.winkingworks.com/zh-TW/') }
+
+  given_a_component { described_class.new(partner: partner) }
+  when_rendered
+
+  it { is_expected.to have_selector('img') }
+  it { is_expected.to have_link }
+end
