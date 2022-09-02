53eb523f78984464b16c8f2ea47980a74ac66daf
蒼時弦也 <elct9620@frost.tw>
2022-09-02 19:43:46 +0800

    Refactor Speaker list to use SpeakerListItemComponent

---
+++ app/components/speaker_list_item_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class SpeakerListItemComponent < ViewComponent::Base
+  with_collection_parameter :speaker
+
+  delegate_missing_to :@speaker
+
+  def initialize(speaker:)
+    super
+    @speaker = speaker
+  end
+end
---
+++ spec/components/speaker_list_item_component_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SpeakerListItemComponent, type: :component do
+  let(:speaker) { create(:speaker, name: '詹承翰') }
+
+  given_a_component { described_class.new(speaker: speaker) }
+  when_rendered
+
+  it { is_expected.to have_link('詹承翰') }
+  it { is_expected.to have_selector('a > img') }
+
+  context 'when speaker title given' do
+    let(:speaker) { create(:speaker, name: '詹承翰', title: '唯晶科技 創辦人暨執行長') }
+
+    it { is_expected.to have_text('唯晶科技 創辦人暨執行長') }
+  end
+end
