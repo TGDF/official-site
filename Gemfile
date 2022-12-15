e3aca7357f68345c2a3d85408d72a9f89679928f
蒼時弦也 <elct9620@frost.tw>
2022-12-15 20:10:51 +0800

    Add jsbundling-rails and remove packs/application.js

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ gem 'devise'
 # Front End
 gem 'cssbundling-rails'
 gem 'friendly_id', '~> 5.4.0'
+gem 'jsbundling-rails'
 gem 'mobility', '~> 1.0'
 gem 'select2-rails'
 gem 'simple_form'
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,4 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+// Entry point for the build script in your package.json
 
 // JotForm
 window.handleIFrameMessage = function(e) {
