af676dc9e42f9a48784e65b712307dce488e3cbb
蒼時弦也 <elct9620@frost.tw>
2022-12-20 14:06:46 +0800

    Remove unused parameter

--- app/helpers/admin/navbar_helper.rb
+++ app/helpers/admin/navbar_helper.rb
@@ -2,7 +2,7 @@
 
 module Admin
   module NavbarHelper
-    def admin_navbar_site_item(name, host, icon: 'cil-square')
+    def admin_navbar_site_item(name, host)
       link_to admin_root_url(host: host), class: 'dropdown-item' do
         concat name
       end
