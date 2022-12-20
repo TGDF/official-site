ed64e6fc25a98cbf5be4fb50bfd6385beb3231fa
蒼時弦也 <elct9620@frost.tw>
2022-12-20 14:00:42 +0800

    Disable unused SVG icon helper

--- app/helpers/admin/navbar_helper.rb
+++ app/helpers/admin/navbar_helper.rb
@@ -4,7 +4,6 @@ module Admin
   module NavbarHelper
     def admin_navbar_site_item(name, host, icon: 'cil-square')
       link_to admin_root_url(host: host), class: 'dropdown-item' do
-        concat svg_icon('media/sprites/free.svg', icon, class: 'c-icon mfe-2')
         concat name
       end
     end
