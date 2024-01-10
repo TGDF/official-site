85aeaa884b87417a58e7de1a14ac988dcc50f709
Aotokitsuruya <contact@aotoki.me>
2024-01-10 20:12:33 +0800

    Add set tenant config for act_as_tenant

--- app/controllers/concerns/tenant_site.rb
+++ app/controllers/concerns/tenant_site.rb
@@ -4,8 +4,18 @@ module TenantSite
   extend ActiveSupport::Concern
 
   included do
+    set_current_tenant_through_filter
+    before_action :set_tenant
+
     helper_method :tenant_site?
     helper_method :current_site
+  end
+
+  def set_tenant
+    return nil if request.host.blank?
+
+    site = Site.find_by(domain: request.host)
+    set_current_tenant(site) if site.present?
   end
 
   def current_site
