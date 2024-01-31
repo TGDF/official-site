a26aa098b2925cbc1156033823549f6dec926072
Aotokitsuruya <contact@aotoki.me>
2024-01-31 20:55:04 +0800

    Add site_id to menu_items to support acts_as_tenant

---
+++ db/migrate/20240131125057_add_site_id_to_menu_items.rb
@@ -0,0 +1,6 @@
+class AddSiteIdToMenuItems < ActiveRecord::Migration[7.0]
+  def change
+    add_column :menu_items, :site_id, :bigint, null: true
+    add_index :menu_items, :site_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_31_124727) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_31_125057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -151,6 +151,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_31_124727) do
     t.boolean "visible", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "site_id"
+    t.index ["site_id"], name: "index_menu_items_on_site_id"
   end
 
   create_table "news", force: :cascade do |t|
