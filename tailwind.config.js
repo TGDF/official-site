330be39338198283a5ffda38d92fc246dd74dc41
Aotokitsuruya <contact@aotoki.me>
2024-06-14 17:37:41 +0800

    Update prose color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,9 +18,22 @@ module.exports = {
       typography: (theme) => ({
         white: {
           css: {
-            '--tw-prose-body': theme('colors.white'),
-            '--tw-prose-counters': theme('colors.white'),
-            '--tw-prose-bullets': theme('colors.white'),
+            '--tw-prose-body': theme('colors.gray[200]'),
+            '--tw-prose-headings': theme('colors.gray[100]'),
+            '--tw-prose-lead': theme('colors.gray[300]'),
+            '--tw-prose-links': theme('colors.gray[100]'),
+            '--tw-prose-bold': theme('colors.gray[100]'),
+            '--tw-prose-counters': theme('colors.gray[400]'),
+            '--tw-prose-bullets': theme('colors.gray[600]'),
+            '--tw-prose-hr': theme('colors.gray[700]'),
+            '--tw-prose-quotes': theme('colors.gray[100]'),
+            '--tw-prose-quote-borders': theme('colors.gray[700]'),
+            '--tw-prose-captions': theme('colors.gray[300]'),
+            '--tw-prose-code': theme('colors.gray[100]'),
+            '--tw-prose-pre-code': theme('colors.gray[900]'),
+            '--tw-prose-pre-bg': theme('colors.gray[100]'),
+            '--tw-prose-th-borders': theme('colors.gray[700]'),
+            '--tw-prose-td-borders': theme('colors.gray[800]'),
           }
         }
       }),
