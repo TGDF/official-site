28b75bf20ec70e6d362ea861c4afb802c9f06d44
Aotokitsuruya <contact@aotoki.me>
2024-06-22 14:12:24 +0800

    Configure sentry profile rate

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -2,6 +2,7 @@
 
 Sentry.init do |config|
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
+  config.profiles_sample_rate = 1.0
   config.traces_sampler = lambda do |sampling_context|
     next sampling_context[:parent_sampled] unless sampling_context[:parent_sampled].nil?
 
