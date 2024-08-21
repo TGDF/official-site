4113d2eba5f0b1c81b4223bfe89a8225c03bc85b
Aotokitsuruya <contact@aotoki.me>
2024-08-21 20:31:17 +0800

    Add ActiveStorage tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_17_123857) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_21_122702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "admin_users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -217,6 +245,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_17_123857) do
     t.datetime "updated_at", null: false
     t.bigint "site_id"
     t.integer "order", default: 0, null: false
+    t.index ["site_id"], name: "index_plans_on_site_id"
   end
 
   create_table "rooms", force: :cascade do |t|
@@ -289,6 +318,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_17_123857) do
     t.index ["site_id", "level_id"], name: "index_sponsors_on_site_id_and_level_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "agenda_times", "agenda_days", column: "day_id"
   add_foreign_key "agendas", "agenda_times", column: "time_id"
   add_foreign_key "agendas", "rooms"
